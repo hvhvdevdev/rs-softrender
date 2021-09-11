8fd2569e5fb56ebd4a18bd55c1a5ed3c20544311
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-09-11 10:35:16 +0700

    struct Pixel and Image

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 pub mod rasterizer;
 pub mod serializer;
 
+pub struct Pixel {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+pub struct Image {
+    pub width: u16,
+    pub height: u16,
+    pub data: Vec<Pixel>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
